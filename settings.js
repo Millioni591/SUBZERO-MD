da3a978112b92e9fb1556a2d71ca58fec0e81a39
Millioni591 <briandaka631@gmail.com>
2025-08-24 03:34:24 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
+  SESSION_ID: "", // SUBZERO-MD;;;2d0zFQgD#Ygbvv1fXJct1Oob_lat7t7LgoP1A2AyiHA5byAWuBys
   OWNER_NUMBER: "263719647303", // Put Owner Number Here 
   PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
